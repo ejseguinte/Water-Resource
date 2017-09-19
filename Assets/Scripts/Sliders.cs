5c94d83c1aa30ca7305c6dba4787e61591c97fe7
Erwin Jay Seguinte <ejseguinte@gmail.com>
2017-09-19 11:23:46 -0700

    Added colors to the effect sliders

---
+++ Assets/Scripts/Sliders.cs
@@ -0,0 +1,50 @@
+﻿ using UnityEngine;
+ using UnityEngine.UI;
+ using System.Collections;
+using System.IO;
+
+[RequireComponent(typeof(Slider))]
+[RequireComponent(typeof(RectTransform))]
+public class Sliders : MonoBehaviour
+{
+
+	private Slider _slider;
+	private float _sliderSize;
+	public Image fill;
+
+	void Awake()
+	{
+		_slider = GetComponent<Slider>();
+		
+	}
+	void Update()
+	{
+		UpdateSliderSense();
+		Debug.Log(_sliderSize);
+		_slider.fillRect.localPosition = new Vector3(0, 0, 0);
+	}
+	public void UpdateSliderSense()
+	{
+		if (_sliderSize == 0)
+		{
+			_sliderSize = GetComponent<RectTransform>().rect.width;
+			_sliderSize = _sliderSize / (_slider.maxValue - _slider.minValue);
+		}
+
+		_slider.fillRect.rotation = new Quaternion(0, 0, 0, 0);
+		_slider.fillRect.pivot = new Vector2(_slider.fillRect.transform.parent.localPosition.x, _slider.fillRect.pivot.y);
+		if (_slider.value > 0)
+		{
+			_slider.fillRect.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, _sliderSize * _slider.value);
+			fill.GetComponent<Image>().color = Color.green;
+		}
+		else
+		{
+			_slider.fillRect.Rotate(0, 0, 180);
+			_slider.fillRect.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, -1 * _sliderSize * _slider.value);
+			fill.GetComponent<Image>().color = Color.red;
+		}
+		_slider.fillRect.localPosition = new Vector3(0, 0, 0);
+	}
+
+}
---
+++ Assets/Scripts/Solver.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Solver : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	public static double Evaluate(string expression)
+     {
+         var doc = new System.Xml.XPath.XPathDocument(new System.IO.StringReader("<r/>"));
+         var nav = doc.CreateNavigator();
+         var newString = expression;
+         newString = (new System.Text.RegularExpressions.Regex(@"([\+\-\*])")).Replace(newString, " ${1} ");
+         newString = newString.Replace("/", " div ").Replace("%", " mod ");
+         return (double)nav.Evaluate("number(" + newString + ")");
+     } 
+}
