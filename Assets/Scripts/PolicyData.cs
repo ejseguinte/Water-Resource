22400fe9f1d4962c590b755f897a8593e2cfa694
Erwin Jay Seguinte <ejseguinte@gmail.com>
2017-10-21 18:43:34 -0700

    Added Policy mechanics

--- Assets/Scripts/PolicyData.cs
+++ Assets/Scripts/PolicyData.cs
@@ -1,13 +1,40 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
+using System;
+using System.Security.Permissions;
 
 public class PolicyData{
 
 	private static Dictionary<string, Policy> _table = new Dictionary<string, Policy>(); //Key  is Year and Month ie 201701: January of 2017
 
 	public static void LoadItemsData(){
-
+		Policy policy = new Policy()
+		{
+			nameID = "test",
+			guiName = "Test",
+			description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam maximus erat sed elit tristique hendrerit. Fusce dictum tortor neque, id vulputate erat tristique at. Aliquam vestibulum tincidunt odio a ultrices. Nullam id iaculis nisl. Ut placerat pharetra vestibulum. Vestibulum quis pharetra magna. Vivamus dapibus mauris quis nisi eleifend hendrerit ac at nulla. Sed et magna eget mauris vehicula finibus. Donec nec venenatis erat."
+		};
+		_table.Add(policy.nameID, policy);
+		
+		Policy policy1 = new Policy()
+		{
+			nameID = "test1",
+			guiName = "Test",
+			description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam maximus erat sed elit tristique hendrerit. Fusce dictum tortor neque, id vulputate erat tristique at. Aliquam vestibulum tincidunt odio a ultrices. Nullam id iaculis nisl. Ut placerat pharetra vestibulum. Vestibulum quis pharetra magna. Vivamus dapibus mauris quis nisi eleifend hendrerit ac at nulla. Sed et magna eget mauris vehicula finibus. Donec nec venenatis erat."
+		};
+		_table.Add(policy1.nameID, policy1);
+		
+		Policy policy2 = new Policy()
+		{
+			nameID = "test2",
+			guiName = "Test",
+			description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam maximus erat sed elit tristique hendrerit. Fusce dictum tortor neque, id vulputate erat tristique at. Aliquam vestibulum tincidunt odio a ultrices. Nullam id iaculis nisl. Ut placerat pharetra vestibulum. Vestibulum quis pharetra magna. Vivamus dapibus mauris quis nisi eleifend hendrerit ac at nulla. Sed et magna eget mauris vehicula finibus. Donec nec venenatis erat."
+		};
+		_table.Add(policy2.nameID, policy2);
+		
+		
 	}
 
 	public static Policy GetItem(string name){
@@ -21,6 +48,14 @@ public class PolicyData{
 			return null;
 		}
 
+	}
+	
+	public static Array GetKeys()
+	{
+		if (_table.Count == 0){
+			LoadItemsData();
+		}
+		return _table.Keys.ToArray();
 	}
 }
 
---
+++ Assets/Scripts/PolicyLogController.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System;
+
+public class PolicyLogController : MonoBehaviour {
+
+	public GameObject textbox;
+	public Transform parent;
+	public Text title;
+	public Text body;
+
+	private Array policies;
+	// Use this for initialization
+	void Start () {
+		
+		policies = PolicyData.GetKeys();
+		float y = 0;
+		parent.transform.position = new Vector2(0, 0);
+		if (policies != null)
+		{
+			foreach (string policyName in policies)
+			{
+				Policy temp = PolicyData.GetItem(policyName);
+				GameObject text = Instantiate(textbox) as GameObject;
+				Text description = text.GetComponentInChildren<Text>();
+				description.text = temp.guiName;
+				text.name = temp.nameID;
+				
+				text.GetComponent<Button>().onClick.AddListener(() => { DisplayPolicy(); }); 
+				text.transform.SetParent(parent.transform);
+				y = y + text.transform.lossyScale.y;
+				Vector2 pos = new Vector2(text.transform.position.x, y);
+				text.transform.position = pos;
+				text.transform.localScale = new Vector3(1f, 1f, 1f);
+			}
+		}
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (parent.GetComponent<VerticalLayoutGroup>().preferredHeight > parent.GetComponent<RectTransform>().sizeDelta.y)
+		{
+			parent.GetComponent<RectTransform>().SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, parent.GetComponent<VerticalLayoutGroup>().preferredHeight);
+		}
+	}
+
+	public void DisplayPolicy()
+	{
+		Policy temp = PolicyData.GetItem(EventSystem.current.currentSelectedGameObject.name);
+		title.text = temp.guiName;
+		body.text = temp.description;
+	}
+	
+}
